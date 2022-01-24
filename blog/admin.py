3b10d5cd4dd42ea966b1d7981e4739fec0623b0d
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-24 14:23:06 +0800

    Add admin side

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,7 @@ from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
 
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
 
-admin.site.register(Post)

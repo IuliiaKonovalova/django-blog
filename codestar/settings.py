fd6a280652c7851553116158ad27efc7379b0746
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-24 13:59:27 +0800

    Create classes for posts and comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-blog-55.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["django-blog-55.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
